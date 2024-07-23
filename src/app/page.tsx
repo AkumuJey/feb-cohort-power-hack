64f791b5f69231908a91e2f4e04fb37e7f712800
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-07-23 23:33:03 +0300

    Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="grow min-h-full bg-teal-300">
+    <main className="min-h-screen bg-teal-300">
       <div className="h-full">DoktariAI</div>
     </main>
   );
